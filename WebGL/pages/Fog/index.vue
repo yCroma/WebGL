cb34197a21f13d3b44aad72d7c3cb89dd4a99e33
yCroma <58209438+yCroma@users.noreply.github.com>
2020-08-07 03:12:33 +0900

    Add module light
    DirectionalLightとAmbientLightをモジュール化した。

---
+++ WebGL/pages/Fog/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="container">
+    <div>
+      <ConstFog />
+    </div>
+  </div>
+</template>
+
+<script>
+import ConstFog from '@/components/Fog/Const'
+
+export default {
+  components: {
+    ConstFog
+  }
+}
+</script>
