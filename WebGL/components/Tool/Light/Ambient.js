296247aac1eb0daa520b3359e7a3c0162e7bfc40
yCroma <58209438+yCroma@users.noreply.github.com>
2020-08-07 03:16:12 +0900

    Revert "Add module light"
    This reverts commit cb34197a21f13d3b44aad72d7c3cb89dd4a99e33.

--- WebGL/components/Tool/Light/Ambient.js
+++
@@ -1,9 +0,0 @@
-import * as THREE from 'three'
-
-export default class Ambient extends THREE.AmbientLight {
-  constructor (color, intensity) {
-    const AmbientColor = color
-    const AmbientIntensity = intensity
-    super(AmbientColor, AmbientIntensity)
-  }
-}
--- WebGL/components/Tool/Light/Drectional.js
+++
@@ -1,9 +0,0 @@
-import * as THREE from 'three'
-
-export default class Directional extends THREE.DirectionalLight {
-  constructor (color, intensity) {
-    const DirectionalColor = color
-    const DirectionalIntensity = intensity
-    super(DirectionalColor, DirectionalIntensity)
-  }
-}
