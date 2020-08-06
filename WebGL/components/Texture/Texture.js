66d9741f6b30c790486480e45403fd522208cf99
yCroma <58209438+yCroma@users.noreply.github.com>
2020-08-06 13:34:53 +0900

    Add module Box & BumpMap
    BoxgeometryをBumpMapしたものを追加した。

--- WebGL/components/Texture/Texture.js
+++ WebGL/components/Texture/Texture.js
@@ -1,6 +1,9 @@
-// import * as THREE from 'three'
+import * as THREE from 'three'
 import Canvas from '@/components/Tool/Canvas'
-import Geometry from '@/components/Texture/Geometry'
+import Box from '@/components/Geometry/Box'
+import BumpMap from '@/components/Tool/Texture/BumpMap'
+import Stone from '@/assets/images/stone.jpg'
+import StoneBump from '@/assets/images/stone-bump.jpg'
 
 export default class Texture {
   constructor (props) {
@@ -10,8 +13,10 @@ export default class Texture {
 
   init () {
     Canvas.init(this.props.$canvas)
-    this.box = new Geometry()
-    Canvas.scene.add(this.box)
+    this.geometry = new Box(5, 5, 5)
+    this.material = new BumpMap(Stone, StoneBump)
+    this.mesh = new THREE.Mesh(this.geometry, this.material)
+    Canvas.scene.add(this.mesh)
     window.addEventListener('resize', this.resize.bind(this))
     this.loop()
   }
@@ -23,8 +28,12 @@ export default class Texture {
   loop () {
     this.render()
 
-    this.box.rotation.x += 0.01
-    this.box.rotation.y += 0.01
+    this.mesh.rotation.x += 0.01
+    this.mesh.rotation.y += 0.01
+    /*
+    this.mesh2.rotation.x += 0.01
+    this.mesh2.rotation.y += 0.01
+    */
 
     requestAnimationFrame(this.loop.bind(this))
   }
