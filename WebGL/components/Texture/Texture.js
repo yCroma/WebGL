3585e0ca28ae3bbc4f6c02aa1f02e89fe3b85b0e
yCroma <58209438+yCroma@users.noreply.github.com>
2020-08-07 03:15:38 +0900

    Update Texture.js
    CanvasからLightを削除したため、表示ファイル側でライトを実装した。

--- WebGL/components/Texture/Texture.js
+++ WebGL/components/Texture/Texture.js
@@ -1,4 +1,7 @@
+// Canvas の追加
 import Canvas from '@/components/Tool/Canvas'
+// Light の追加
+import Directional from '@/components/Tool/Light/Drectional'
 import Blueprint from '@/components/Texture/Blueprint'
 
 export default class Texture {
@@ -11,6 +14,10 @@ export default class Texture {
     Canvas.init(this.props.$canvas)
     // camera の調整
     Canvas.camera.position.set(0, 0, 20)
+    // Light の追加
+    this.directional = new Directional(0xFFFFFF, 1.0)
+    this.directional.position.set(1, 1, 1)
+    Canvas.scene.add(this.directional)
     // Blueprint内にgeometryとmaterialの情報がまとまっている
     this.mesh = new Blueprint()
     this.mesh.position.set(0, 0, 0)
